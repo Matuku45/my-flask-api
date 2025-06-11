36489195593dceb4685d538e4b7944e58f43247d
Matuku45 <150783819+Matuku45@users.noreply.github.com>
2025-06-11 21:22:11 +0200

    Update main.py

--- main.py
+++ main.py
@@ -82,6 +82,6 @@ def delete_student(student_id):
 # Run the app
 # ------------------------
 
+
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run()
